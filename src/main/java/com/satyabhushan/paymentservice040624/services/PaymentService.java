a1feab9333e06cd6c76f798788f34fe0e2967454
789143 <vijayadurga278@gmail.com>
2024-06-06 00:07:48 +0530

    Payment Service initial commit

--- src/main/java/com/satyabhushan/paymentservice040624/services/PaymentService.java
+++ src/main/java/com/satyabhushan/paymentservice040624/services/PaymentService.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PaymentService {
+public class PaymentService{
 
     private final PaymentGateway paymentGateway ;
 
